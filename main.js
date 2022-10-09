23806601385339b4d5fd7b84d0b0aee4fee03dd2
Shubham Vora <shubhamv3108@gmail.com>
2022-10-09 19:43:08 +0530

    Todo app in express.js

---
+++ main.js
@@ -0,0 +1,231 @@
+const express = require('express')
+const session = require("express-session");
+const app = express()
+const fs = require("fs");
+const port = 3000
+
+
+app.use(express.static("public"));
+app.use(express.json()); //middleware used to parse json data
+app.use(express.urlencoded({extended:true})); //middleware used to parse form data
+app.use(session({
+	secret:'keyboard cat',
+	resave:false,
+	saveUninitialized:true,
+	cookie:{secure:false}
+}))
+
+app.get('/', Home);
+app.get("/about",About);
+app.route("/todo").get(GetTodo).post(PostTodo);
+app.post("/delete",DeleteTodo);
+app.post("/edit",EditTodo);
+app.post("/complete",CompleteTodo);
+
+
+app.route("/login").get(function(req,res){
+	res.sendFile(__dirname+"/public/html/login.html")
+}).post(function(req,res){
+	// console.log(req.body);
+	getUser(function(users){
+		const user = users.filter(function(user){
+			if(user.username===req.body.username && user.password===req.body.password){
+				return true
+			}
+		})
+		if(user.length){
+			req.session.isLoggedIn = true;
+			res.redirect('/');
+		}
+		else{
+			res.redirect('/login');
+		}
+	})
+	// res.sendFile(__dirname+"/public/html/index.html");
+});
+
+app.route("/signup").get(function(req,res){
+	res.sendFile(__dirname+"/public/html/signup.html")
+}).post(function(req,res){
+	console.log(req.body);
+	saveUser(req.body,function(err){
+		if(err){
+			res.end("Something Went Horribly Wrong!");
+		}
+		else{
+			res.redirect('/login');
+		}
+	})
+});
+
+app.route("/logout").get(function(req,res){
+	req.session.destroy();
+	res.redirect("/login");
+})
+
+
+app.listen(port, () => {
+	console.log(`Server is Live running at http://localhost:${port}`);
+})
+
+function Home(req,res){
+	if(req.session.isLoggedIn){
+		res.sendFile(__dirname+"/public/html/index.html");
+	}
+	else{
+		res.redirect("/login");
+	}
+}
+
+function About(req,res){
+	res.sendFile(__dirname+"/public/html/about.html");
+}
+
+function GetTodo(req,res){
+	console.log("In GetTodo");      //1
+	getTodos(function(err,data){
+		console.log("in getTodos");    //4
+		//console.log(res.json(data));
+		res.json(data);
+	})
+}
+
+function PostTodo(req,res){
+	console.log("In Post Todo");    //5
+	saveTodos(req.body,function(){
+		console.log("in saveTodos");   //10
+		res.end();
+	})
+}
+
+function DeleteTodo(req,res){
+	console.log("In Delete Todo");
+	console.log("req body: ",req.body);
+	delTodos(req.body,function(){
+		console.log("in delTodos");
+		res.end();
+	})
+}
+
+function EditTodo(req,res){
+	console.log("In Edit Todo");
+	console.log("req body: ",req.body);
+	editTodos(req.body,function(){
+		console.log("in editTodos");
+		res.end();
+	})
+}
+
+function CompleteTodo(req,res){
+	console.log("In Complete Todo");
+	console.log("req body: ",req.body);
+	compTodos(req.body,function(){
+		console.log("in compTodos");
+		res.end();
+	})
+}
+
+
+function getTodos(callback){
+	console.log("reading file");   //2 //6
+	fs.readFile("./dbfile.txt","utf-8",function(err,data){
+		if (err){
+			callback(err,null);
+			return;
+		}
+		console.log("data read",data);   //3  //7
+		console.log("sending this ",JSON.parse(data));
+		callback(null,JSON.parse(data));
+	})
+}
+
+
+function saveTodos(todo,callback){
+	getTodos(function(err,todos){
+		//console.log("pushing todo and writing todo"); //8
+		todos.push(todo);
+		//console.log("todo , todos:",todo,todos); //9
+		fs.writeFile("./dbfile.txt",JSON.stringify(todos),function(err){
+			if(err){
+				callback(err,null);
+				return;
+			}
+			callback(null);
+		})
+	})
+}
+
+function delTodos(todo,callback){
+	getTodos(function(err,todos){
+		console.log("getting todos");
+		console.log("B4 todo, todos: ",todo,todos);
+		todos.splice(todos.findIndex(a => a.text === todo.text) , 1);
+		console.log("After todo,todos:",todo,todos);
+		//console.log("removed "+todo+" from db");
+		fs.writeFile("./dbfile.txt",JSON.stringify(todos),function(err){
+			if(err){
+				callback(err,null);
+				return;
+			}
+			callback(null);
+		})
+	})
+}
+
+function editTodos(todo,callback){
+	getTodos(function(err,todos){
+		console.log("editingg..");	
+		console.log("B4 todo, todos: ",todo,todos);
+		var index = todos.findIndex(a=>a.text===todo.text);
+		todos[index].text = todo.ntext;
+		console.log("after todos: ",todos);
+		fs.writeFile("./dbfile.txt",JSON.stringify(todos),function(err){
+			if(err){
+				callback(err,null);
+				return;
+			}
+			callback(null);
+		})
+	})
+	// callback(null);
+}
+
+function compTodos(todo,callback){
+	getTodos(function(err,todos){
+		console.log("completing..");
+		console.log("B4 todo,todos: ",todo,todos);
+		var index = todos.findIndex(a=>a.text==todo.text);
+		try{
+
+		todos[index].strike = true;
+		}catch(err){
+			console.log(err);
+			// alert("invalid action");
+		}
+		console.log("after compl: ",todos);
+		fs.writeFile("./dbfile.txt",JSON.stringify(todos),function(err){
+			if(err){
+				callback(err,null);
+				return;
+			}
+			callback(null);
+		})
+	})
+}
+
+function getUser(callback){
+	fs.readFile("./users.txt","utf-8",function(err,data){
+		if(data){
+			callback(JSON.parse(data));
+		}
+	})
+}
+
+function saveUser(user,callback){
+	getUser(function(users){
+		users.push(user);
+		fs.writeFile("./users.txt",JSON.stringify(users),function(){
+			callback();
+		})
+	})
+}	
---
+++ public/js/myscript.js
@@ -0,0 +1,180 @@
+var table = document.querySelector("#table1");
+const ip = document.getElementById("ip");
+var strike=false;
+const save= document.getElementById("save");
+var fromBut=false;
+ip.addEventListener("keypress",function(event){
+  if(event.key=="Enter"){
+  fromBut=true;
+  const value = ip.value;
+  const request = new XMLHttpRequest();
+  console.log("Inside save, sending post request");
+  request.open("POST","http://localhost:3000/todo");
+  request.setRequestHeader("Content-Type","application/json");
+  request.send(JSON.stringify({text:value,strike:strike}));
+
+  request.addEventListener("load",function(){
+    console.log("in request event listener");
+    if(request.status===200){
+      taskDisplay(value,fromBut);
+      ip.value='';
+
+      //TODO add request Listener
+     }
+    else{
+      console.log("error occured");
+    }
+  })
+}
+})
+
+
+const getAllTodoRequests = new XMLHttpRequest();
+console.log("Sending get request");
+getAllTodoRequests.open("GET","http://localhost:3000/todo");
+getAllTodoRequests.send();
+
+getAllTodoRequests.addEventListener("load",function(){
+  fromBut=false;
+  console.log("in getAllTodoRequests event listener");
+  if (getAllTodoRequests.status===200){
+    var todos = JSON.parse(getAllTodoRequests.responseText);
+    todos.forEach(function(value){
+      console.log(value);
+      taskDisplay(value,fromBut)
+    })
+  }
+  else{
+    console.log("error in getting all");
+  }
+})
+
+function taskDisplay(value,fromBut){
+  var Row = document.createElement("tr");
+  var celltxt = document.createElement("td");
+  var celltxt2 = document.createElement("td");
+  var compButton = document.createElement("button");
+  var delButton = document.createElement("button");
+  var editButton = document.createElement("button");
+  var delIco = document.createElement("i");
+  var editIco = document.createElement("i");
+  var compIco = document.createElement("i");
+
+  delIco.className="fa fa-trash";
+  editIco.className="fa fa-pencil";
+  compIco.className = "fa fa-check";
+  
+  // compButton.innerText = "Done";
+  // // delButton.innerText = "Delete";
+  // editButton.innerText = "Edit";
+  
+
+  if (fromBut){
+  celltxt.innerText = value;
+  celltxt.classList.add("p");
+  }
+  else{
+    celltxt.innerText = value.text;
+    if(value.strike===true){
+      celltxt.style.textDecoration = "line-through";
+    }
+    celltxt.classList.add("p");
+  }
+  delButton.appendChild(delIco);
+  editButton.appendChild(editIco);
+  compButton.appendChild(compIco);
+  celltxt2.appendChild(compButton);
+  celltxt2.appendChild(editButton);
+  celltxt2.appendChild(delButton);
+  Row.appendChild(celltxt);
+  Row.appendChild(celltxt2);
+  table.appendChild(Row);
+
+  // parent.appendChild(elem);
+  // parent.appendChild(btn);
+  delButton.addEventListener("click",function(){
+    deleter(value,fromBut,Row)
+    });
+  editButton.addEventListener("click",function(){
+    editor(value,fromBut,celltxt);
+  });
+  compButton.addEventListener("click",function(){
+    completer(value,fromBut,strike,celltxt);
+  })
+}
+
+
+function deleter(value,fromBut,Row){
+  const delRequest = new XMLHttpRequest();
+  console.log("inside delete btn event listner");
+  delRequest.open("POST","http://localhost:3000/delete");
+  delRequest.setRequestHeader("Content-Type","application/json");
+  console.log(value);
+  if (fromBut){
+    delRequest.send(JSON.stringify({text:value}));
+  }
+  else{
+  delRequest.send(JSON.stringify({text:value.text}));
+  }
+
+  delRequest.addEventListener("load",function(){
+    console.log("bring back after delete");
+    if (delRequest.status===200){
+    table.removeChild(Row);
+    
+  }
+  else{
+    console.log("error in getting all");
+  }
+})
+    // taskDisplay(value,fromBut);
+    // window.location.reload();
+ }
+
+function editor(value,fromBut,celltxt){
+  const editRequest = new XMLHttpRequest();
+  console.log("in edit btn");
+  editRequest.open("POST","http://localhost:3000/edit");
+  editRequest.setRequestHeader("Content-Type","application/json");
+  console.log(value);
+  var newValue = prompt("enter new value: ");
+  if(fromBut){
+    editRequest.send(JSON.stringify({text:value,ntext:newValue}));
+  }
+  else{
+    editRequest.send(JSON.stringify({text:value.text,ntext:newValue}));
+  }
+  editRequest.addEventListener("load",function(){
+    console.log("bring back after edit");
+    if (editRequest.status===200){
+    celltxt.innerText = newValue;
+    
+    }
+    else{
+      console.log("error in getting all");
+    }
+  })
+}
+
+function completer(value,fromBut,strike,celltxt){
+  const compRequest = new XMLHttpRequest();
+  console.log("in Comp button");
+  compRequest.open("POST","http://localhost:3000/complete");
+  compRequest.setRequestHeader("Content-Type","application/json");
+  console.log(value);
+  if(fromBut){
+    compRequest.send(JSON.stringify({text:value,strike:strike}));
+  }
+  else{
+    compRequest.send(JSON.stringify({text:value.text,strike:strike}));
+  }
+  compRequest.addEventListener("load",function(){
+    console.log("bring back after COMPLETE");
+    if (compRequest.status===200){
+      celltxt.style.textDecoration = "line-through";
+  }
+  else{
+    console.log("error in getting all");
+  }
+  })
+}
