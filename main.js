e37a2995702b726f28c4e2307865a9914e6e983d
Shubham Vora <shubhamv3108@gmail.com>
2022-10-09 20:51:08 +0530

    fixed port number issue

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require('express')
 const session = require("express-session");
 const app = express()
 const fs = require("fs");
-const port = 3000
+const port = process.env.PORT||5000;
 
 
 app.use(express.static("public"));
